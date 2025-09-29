6e7a5b2c0298acf80918df4d080d1b5dcfc00efc
Florent Cardoen <florent.cardoen@azimut.be>
2025-09-29 22:58:06 +0200

    Update image paths in index.html and HeroSlider.ts to use relative paths for consistency across the project.

--- modern-site/src/components/HeroSlider.ts
+++ modern-site/src/components/HeroSlider.ts
@@ -10,12 +10,12 @@ export class HeroSlider {
 
     // Original slide images from the theme
     this.images = [
-      { name: "img 7", src: '/img/img7.jpeg' },
-      { name: "img 15", src: '/img/img15.jpeg' },
-      { name: "img 16", src: '/img/img16.jpeg' },
-      { name: "img 18", src: '/img/img18.jpeg' },
-      { name: "img 19", src: '/img/img19.jpeg' },
-      { name: "img 22", src: '/img/img22.jpeg' }
+      { name: "img 7", src: './img/img7.jpeg' },
+      { name: "img 15", src: './img/img15.jpeg' },
+      { name: "img 16", src: './img/img16.jpeg' },
+      { name: "img 18", src: './img/img18.jpeg' },
+      { name: "img 19", src: './img/img19.jpeg' },
+      { name: "img 22", src: './img/img22.jpeg' }
     ]
 
     this.init()
